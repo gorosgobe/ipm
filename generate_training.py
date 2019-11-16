e4798eabfc8ce9452cd3e1a64bb45d67d606a4f8
pablo <pablo.gobe@gmail.com>
2019-11-16 13:05:14 +0000

    Extracted robot object to handle restoration of initial position

--- generate_training.py
+++ generate_training.py
@@ -3,64 +3,28 @@ import numpy as np
 from pyrep import PyRep
 from pyrep.backend import vrep
 from pyrep.robots.arms.sawyer import Sawyer
+from pyrep.robots.end_effectors.baxter_gripper import BaxterGripper
 from os.path import join, dirname, abspath
 
-SCENE_FILE = join(dirname(abspath(__file__)), "scenes/sawyer_reach_cube.ttt")
+from sawyer_robot import SawyerRobot
 
-def get_vision_sensor():
-    return vrep.simGetObjectHandle("sawyer_vision_sensor")
+SCENE_FILE = join(dirname(abspath(__file__)), "scenes/sawyer_reach_cube.ttt")
 
 def save_images(images, prefix=""):
     for idx, img in enumerate(images):
         img = cv2.convertScaleAbs(img, alpha=(255.0))
         cv2.imwrite("{}image{}.png".format(prefix, idx), img)
 
-def simulate_path(pr, path, sawyer):
-    path.visualize()
-    path.set_to_start()
-    pr.step()
-
-    vision_sensor = get_vision_sensor()
-    resolution = vrep.simGetVisionSensorResolution(vision_sensor)
-
-    tip_positions = []
-    images = []
-    done = False
-    while not done:
-        done = path.step()
-        pr.step()
-        tip_positions.append(sawyer.get_tip().get_position())
-        images.append(vrep.simGetVisionSensorImage(vision_sensor, resolution))
-
-    return tip_positions, images
-
 pr = PyRep()
 pr.launch(SCENE_FILE, headless=False)
 pr.start()
 
-sawyer = Sawyer()
-tip = sawyer.get_tip()
-tip_position = tip.get_position()
-print(tip_position)
+sawyer_robot = SawyerRobot(pr)
+tip_positions, images = sawyer_robot.run_simulation()
 
-target_cube = vrep.simGetObjectHandle("target_cube")
-target_cube_position = vrep.simGetObjectPosition(target_cube, -1)
-print(target_cube_position)
-
-sawyer_vision_sensor = vrep.simGetObjectHandle("sawyer_vision_sensor")
-sawyer_vision_sensor_position = vrep.simGetObjectPosition(sawyer_vision_sensor, -1)
-print(sawyer_vision_sensor_position)
-
-# Move vision sensor to tip of Sawyer, tip is parent so from now on can use sim_handle_parent as relativeHandle
-vrep.simSetObjectPosition(sawyer_vision_sensor, -1, tip_position)
-sawyer_vision_sensor_position = vrep.simGetObjectPosition(sawyer_vision_sensor, -1)
-print(sawyer_vision_sensor_position)
-
-path = sawyer.get_path(position=target_cube_position, euler=[0.0, -np.pi, 0.0])
-print(path)
-tips, images = simulate_path(pr, path, sawyer)
 save_images(images)
-
+sawyer_robot.run_simulation()
+sawyer_robot.run_simulation()
 
 pr.stop()
 pr.shutdown()
---
+++ sawyer_robot.py
@@ -0,0 +1,136 @@
+import numpy as np
+from pyrep import PyRep
+from pyrep.backend import vrep
+from pyrep.robots.arms.sawyer import Sawyer
+from pyrep.robots.end_effectors.baxter_gripper import BaxterGripper
+
+class WristCamera(object):
+    VISION_SENSOR = "Sawyer_wristCamera"
+    def __init__(self):
+        self.vision_sensor_handle = vrep.simGetObjectHandle(WristCamera.VISION_SENSOR)
+        self.resolution = vrep.simGetVisionSensorResolution(self.vision_sensor_handle)
+
+    def get_image(self):
+        return vrep.simGetVisionSensorImage(self.vision_sensor_handle, self.resolution)
+
+class SawyerRobot(object):
+
+    SAWYER        = "Sawyer"
+    JOINT1        = "Sawyer_joint1"
+    JOINT2        = "Sawyer_joint2"
+    JOINT3        = "Sawyer_joint3"
+    JOINT4        = "Sawyer_joint4"
+    JOINT5        = "Sawyer_joint5"
+    JOINT6        = "Sawyer_joint6"
+    JOINT7        = "Sawyer_joint7"
+    JOINTS        = [
+        JOINT1, JOINT2, JOINT3, JOINT4, JOINT5, JOINT6, JOINT7
+    ]
+    TARGET_CUBE   = "target_cube"
+    # looking downwards from initial position upwards
+    LOOK_DOWNWARDS_POSITION = { 
+        2: -1.3, 
+        4: 1.4, 
+        6: 1.4 
+    }
+
+    def __init__(self, pr, move_to_initial_position=True):
+        """
+        pr: PyRep instance, scene must have been launched and simulation 
+        must have been started
+        """
+        self.pr      = pr
+        self.sawyer  = Sawyer()
+        self.gripper = BaxterGripper()
+        self.tip_velocities = []
+        self.sawyer_handle = vrep.simGetObjectHandle(SawyerRobot.SAWYER)
+        self.camera = WristCamera()
+        self.target_cube_handle = vrep.simGetObjectHandle(SawyerRobot.TARGET_CUBE)
+        # Handles for every joint
+        self.joint_handles = [vrep.simGetObjectHandle(joint_name) for joint_name in SawyerRobot.JOINTS]
+
+        # Move robot to initial position in simulation
+        if move_to_initial_position:
+            self.set_initial_position()
+        # Initial angles for all robot joints. If move_to_initial_position, these will be the ones after
+        # moving the robot
+        self.joint_initial_state = {
+            joint_number + 1: vrep.simGetJointPosition(joint_handle) for joint_number, joint_handle in enumerate(self.joint_handles)
+        }
+
+    @staticmethod
+    def move_joint(joint_handle, target_angle):
+        vrep.simSetJointTargetPosition(joint_handle, target_angle)
+
+    def move_sawyer_to(self, position, orientation):
+        vrep.simSetObjectPosition(self.sawyer_handle, -1, position)
+        vrep.simSetObjectOrientation(self.sawyer_handle, -1, orientation)
+
+    def get_joint_handle(self, joint_number):
+        """
+        joint_number: int, ranging from 1 to 7
+        """
+        if joint_number not in range(1, 8):
+            raise Exception("Joint number should be between 1 and 7")
+        return self.joint_handles[joint_number - 1]
+
+    def set_initial_position(self):
+        self.set_angles(SawyerRobot.LOOK_DOWNWARDS_POSITION)
+
+    def set_angles(self, angles):
+        """
+        angles: dictionary of (joint_number, angle) mappings
+        """
+        for joint_number in angles:
+            SawyerRobot.move_joint(self.get_joint_handle(joint_number), angles[joint_number])
+
+        done = False
+        while not done:
+            prev_pos = np.array(self.sawyer.get_tip().get_position())
+            self.pr.step()
+            curr_pos = np.array(self.sawyer.get_tip().get_position())
+            done = done or np.linalg.norm(curr_pos - prev_pos) < 1e-4
+
+    def _simulate_path(self, path):
+        path.visualize()
+        path.set_to_start()
+        self.pr.step()
+
+        tip_positions = []
+        images = []
+        done = False
+        while not done:
+            done = path.step()
+            self.pr.step()
+            tip_positions.append(self.sawyer.get_tip().get_position())
+            images.append(self.camera.get_image())
+
+        return tip_positions, images
+
+
+    def run_simulation(
+            self,
+            offset_position=[0.0, 0.0, 0.0], 
+            offset_orientation=[0.0, 0.0, 0.0],
+            move_to_initial_position=True
+        ):
+
+        if move_to_initial_position:
+            # move all angles back to original position, as specified by
+            # moving to default position and then to looking downwards position
+            self.set_angles(self.joint_initial_state)
+
+        sawyer_position = vrep.simGetObjectPosition(self.sawyer_handle, -1)
+        sawyer_orientation = vrep.simGetObjectOrientation(self.sawyer_handle, -1)
+        new_p = sawyer_position + np.asarray(offset_position)
+        new_orient = sawyer_orientation + np.asarray(offset_orientation)
+
+        print("Moving to: {}".format(new_p))
+        self.move_sawyer_to(list(new_p), list(new_orient))
+
+        target_cube_position = vrep.simGetObjectPosition(self.target_cube_handle, -1)
+        target_cube_position_above = np.array(target_cube_position) + np.array([0.0, 0.0, 0.08])
+        path = self.sawyer.get_path(position=list(target_cube_position_above), euler=[0.0, 0.0, 0.0])
+        tips, images = self._simulate_path(path)
+
+        return tips, images
