370865877e81351be44a3865333fc3bdfe9aadb8
gorosgobe <pablo.gobe@gmail.com>
2020-01-25 16:02:08 +0000

    Fix setup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@
 # to be run from ipm/ directory
 python3 -m venv venv
 source venv/bin/activate
-pip3 install numpy torch torchvision pytorch-ignite python-opencv pandas imageio matplotlib
+pip3 install numpy torch torchvision pytorch-ignite opencv-python pandas imageio matplotlib
+pip3 uninstall -y torch
+pip3 install torch
