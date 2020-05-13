6c5ff2547f7bcee037df1ef65f4c3119038246d7
gorosgobe <pablo.gobe@gmail.com>
2020-05-13 18:38:35 +0100

    Modify choice parameter to be range - otherwise we cannot plot width and height :(

--- lib/bop/crop_size_feature_search.py
+++ lib/bop/crop_size_feature_search.py
@@ -1,3 +1,5 @@
+import json
+
 import numpy as np
 from ax import optimize
 from ax.plot.contour import plot_contour
@@ -28,19 +30,25 @@ class CropSizeFeatureSearch(Saveable):
         max_width, max_height = self.max
         width = round(min_width + (max_width - min_width) * parameterization["width"])
         height = round(min_height + (max_height - min_height) * parameterization["height"])
-        index = parameterization["feature"]
+        # not ideal, better to have a choice parameter, but then Sobol is used and can't plot, visualise x, y
+        index = round(parameterization["feature"])
         print(f"Trial for feature {index}, size ({width}, {height})")
         val_loss = self.dsae_feature_chooser.train_model_with_feature(
-            index=parameterization["feature"],
+            index=index,
             crop_size=(width, height)
         )
 
         return val_loss
 
+    @staticmethod
+    def get_plot_str(data_dict):
+        # because for some reason, people writing Ax did not think
+        return f"data = json.loads(\'{json.dumps(data_dict, indent=2)}\')"
+
     def search(self, total_trials):
         best_parameters, values, experiment, model = optimize(
             parameters=[
-                {"name": "feature", "type": "choice", "values": list(range(self.features))},
+                {"name": "feature", "type": "range", "bounds": [0.0, float(self.features - 1)]},
                 {"name": "width", "type": "range", "bounds": [0.0, 1.0]},
                 {"name": "height", "type": "range", "bounds": [0.0, 1.0]}
             ],
@@ -61,11 +69,11 @@ class CropSizeFeatureSearch(Saveable):
         self.values = values
         self.experiment = object_to_json(experiment)
         # cannot store model directly
-        best_contour_config = plot_contour(model=model, param_x="width", param_y="height", metric_name='hartmann6',
+        best_contour_config = plot_contour(model=model, param_x="width", param_y="height", metric_name="objective",
                                            slice_values=dict(
-                                               feature=best_parameters["feature"]
+                                               feature=round(best_parameters["feature"])
                                            ))
-        self.best_plot_contour = plot_config_to_html(best_contour_config)
+        self.best_plot_contour = self.get_plot_str(best_contour_config.data)
         best_objectives = np.array(
             [[trial.objective_mean * 100 for trial in experiment.trials.values()]])
         improvement_plot = optimization_trace_single_method(
@@ -73,7 +81,7 @@ class CropSizeFeatureSearch(Saveable):
             title="Validation loss vs. # of iterations",
             ylabel="Validation loss",
         )
-        self.improvement_plot = plot_config_to_html(improvement_plot)
+        self.improvement_plot = self.get_plot_str(improvement_plot)
 
     def get_info(self):
         return dict(
@@ -86,8 +94,8 @@ class CropSizeFeatureSearch(Saveable):
         )
 
     def save_plots(self, path):
-        with open(path + "_contour.html", "w+") as f:
+        with open(path + "_contour.plot_str", "w+") as f:
             f.write(self.best_plot_contour)
 
-        with open(path + "_improvement.html", "w+") as f:
+        with open(path + "_improvement.plot_str", "w+") as f:
             f.write(self.improvement_plot)
