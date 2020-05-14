01ce69781570adf0cfd2f50bef44a73a0f4699ca
gorosgobe <pablo.gobe@gmail.com>
2020-05-14 11:48:07 +0100

    Add search for crop size on specific feature, re-add choice parameters, fix issue with improvement plot.

--- lib/bop/crop_size_feature_search.py
+++ lib/bop/crop_size_feature_search.py
@@ -11,12 +11,14 @@ from lib.common.saveable import Saveable
 
 
 class CropSizeFeatureSearch(Saveable):
-    def __init__(self, name, latent_dimension, dsae_feature_chooser, max=(128, 96), min=(32, 24)):
+    def __init__(self, name, latent_dimension, dsae_feature_chooser, max=(128, 96), min=(24, 24)):
         super().__init__()
         self.name = name
         self.features = latent_dimension // 2
         self.dsae_feature_chooser = dsae_feature_chooser
         self.max = max
+        # AttentionNetworkCoordGeneral can handle images as small as (24, 24)
+        # TODO: Attention Coord without strides
         self.min = min
 
         self.best_parameters = None
@@ -25,13 +27,13 @@ class CropSizeFeatureSearch(Saveable):
         self.best_plot_contour = None
         self.improvement_plot = None
 
-    def evaluation_function(self, parameterization):
+    def evaluation_function(self, parameterization, index):
         min_width, min_height = self.min
         max_width, max_height = self.max
         width = round(min_width + (max_width - min_width) * parameterization["width"])
         height = round(min_height + (max_height - min_height) * parameterization["height"])
-        # not ideal, better to have a choice parameter, but then Sobol is used and can't plot, visualise x, y
-        index = round(parameterization["feature"])
+        if index is None:
+            index = parameterization["feature"]
         print(f"Trial for feature {index}, size ({width}, {height})")
         val_loss = self.dsae_feature_chooser.train_model_with_feature(
             index=index,
@@ -45,14 +47,24 @@ class CropSizeFeatureSearch(Saveable):
         # because for some reason, people writing Ax did not think
         return f"data = json.loads(\'{json.dumps(data_dict, indent=2)}\')"
 
-    def search(self, total_trials):
-        best_parameters, values, experiment, model = optimize(
-            parameters=[
-                {"name": "feature", "type": "range", "bounds": [0.0, float(self.features - 1)]},
+    def search(self, total_trials, index=None):
+        # if index is not None, ran search of crop size for that specific index
+        parameters = [
                 {"name": "width", "type": "range", "bounds": [0.0, 1.0]},
                 {"name": "height", "type": "range", "bounds": [0.0, 1.0]}
-            ],
-            evaluation_function=lambda params: self.evaluation_function(params),
+        ]
+
+        if index is None:
+            feature_param = dict(
+                name="feature",
+                type="choice",
+                values=list(range(self.features))
+            )
+            parameters.append(feature_param)
+
+        best_parameters, values, experiment, model = optimize(
+            parameters=parameters,
+            evaluation_function=lambda params: self.evaluation_function(params, index),
             total_trials=total_trials,
             minimize=True
         )
@@ -69,11 +81,16 @@ class CropSizeFeatureSearch(Saveable):
         self.values = values
         self.experiment = object_to_json(experiment)
         # cannot store model directly
-        best_contour_config = plot_contour(model=model, param_x="width", param_y="height", metric_name="objective",
-                                           slice_values=dict(
-                                               feature=round(best_parameters["feature"])
-                                           ))
-        self.best_plot_contour = self.get_plot_str(best_contour_config.data)
+        try:
+            best_contour_config = plot_contour(model=model, param_x="width", param_y="height", metric_name="objective",
+                                               slice_values=dict(
+                                                   feature=round(best_parameters["feature"])
+                                               ))
+            self.best_plot_contour = self.get_plot_str(best_contour_config.data)
+        except Exception:
+            # model may be Sobol, lacking a predict method
+            pass
+
         best_objectives = np.array(
             [[trial.objective_mean * 100 for trial in experiment.trials.values()]])
         improvement_plot = optimization_trace_single_method(
@@ -81,7 +98,7 @@ class CropSizeFeatureSearch(Saveable):
             title="Validation loss vs. # of iterations",
             ylabel="Validation loss",
         )
-        self.improvement_plot = self.get_plot_str(improvement_plot)
+        self.improvement_plot = self.get_plot_str(improvement_plot.data)
 
     def get_info(self):
         return dict(
@@ -94,8 +111,9 @@ class CropSizeFeatureSearch(Saveable):
         )
 
     def save_plots(self, path):
-        with open(path + "_contour.plot_str", "w+") as f:
-            f.write(self.best_plot_contour)
+        if self.best_plot_contour is not None:
+            with open(path + "_contour.plot_str", "w+") as f:
+                f.write(self.best_plot_contour)
 
         with open(path + "_improvement.plot_str", "w+") as f:
             f.write(self.improvement_plot)
