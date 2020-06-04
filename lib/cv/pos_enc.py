f3c9239f09551f660d877d87fa5161be95f1a7c2
gorosgobe <pablo.gobe@gmail.com>
2020-06-04 17:31:51 +0100

    Move to GPU

--- lib/cv/pos_enc.py
+++ lib/cv/pos_enc.py
@@ -12,7 +12,7 @@ class PositionalEncodings(object):
     def get_positional_encodings(L, batched_coord_maps):
         b, c, h, w = batched_coord_maps.size()
         assert c == 2
-        enc_range = torch.arange(L)
+        enc_range = torch.arange(L).to(batched_coord_maps.device)
         exp = (2 ** enc_range) * np.pi
         # Here we have (B, C=2, H, W) and (L,)
         # Get (B, C, 1, H, W) from (B, C, H, W) and (B, C, L, H, W) from (L)
